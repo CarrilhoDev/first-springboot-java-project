f114f01d7d019fcdbf8a4d309c8b8bd107eb9287
CarrilhoDev <carlos.carrilho@hotmail.com>
2020-05-31 18:17:16 -0400

    Payment class, one-to-one association

--- src/main/java/com/carrilho/firstProject/config/TestConfig.java
+++ src/main/java/com/carrilho/firstProject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.carrilho.firstProject.entities.Category;
 import com.carrilho.firstProject.entities.Order;
 import com.carrilho.firstProject.entities.OrderItem;
+import com.carrilho.firstProject.entities.Payment;
 import com.carrilho.firstProject.entities.Product;
 import com.carrilho.firstProject.entities.User;
 import com.carrilho.firstProject.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
