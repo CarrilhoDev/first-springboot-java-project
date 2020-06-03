c471ccdc761b0723aba8c1c1ee17a9be8f5a6af2
CarrilhoDev <carlos.carrilho@hotmail.com>
2020-06-03 00:10:06 -0400

    Making it better

--- src/main/java/com/carrilho/firstProject/services/OrderService.java
+++ src/main/java/com/carrilho/firstProject/services/OrderService.java
@@ -3,11 +3,17 @@ package com.carrilho.firstProject.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.carrilho.firstProject.entities.Order;
 import com.carrilho.firstProject.repositories.OrderRepository;
+import com.carrilho.firstProject.services.exceptions.DatabaseException;
+import com.carrilho.firstProject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -21,7 +27,35 @@ public class OrderService {
 
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	public Order insert(Order obj) {
+		return repository.save(obj);
 	}
 
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Order update(Long id, Order obj) {
+		try {
+			Order entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setMoment(obj.getMoment());
+		entity.setOrderStatus(obj.getOrderStatus());
+		entity.setClient(obj.getClient());
+	}
 }
